b509dd85f069ad4ba8f6056b705431b707851772
Tim Joosten <Tjoosten3@gmail.com>
2016-11-11 15:18:12 +0100

    Add applications model

--- application/models/Applications.php
+++ application/models/Applications.php
@@ -1,1 +1,30 @@
-git add
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class Applications
+ */
+Class Applications extends Eloquent
+{
+    /**
+     * Set the database connection;
+     *
+     * @var string
+     */
+    public $connection = 'auth';
+
+    /**
+     * The database table.
+     *
+     * @var string
+     */
+    protected $table = 'Platforms';
+
+    /**
+     * Mass-assign fields
+     *
+     * @var array
+     */
+    protected $fillable = [];
+}
