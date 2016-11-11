2a95d4bd9331369c50164d84ca8b90513690cc51
Tim Joosten <Tjoosten3@gmail.com>
2016-11-11 16:42:28 +0100

    Add the profile controller

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -1,17 +1,40 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Class Account
+ */
 class Account extends MY_Controller
 {
+    /**
+     * The logged in session data array.
+     */
+    public $Session = [];
+
+    /**
+     * Account constructor.
+     */
     public function __construct()
     {
         parent::__construct();
-        $this->load->library(['blade']);
+        $this->load->helper(['url']);
+        $this->load->library(['blade', 'session', 'form_validation']);
         $this->load->model('Logins', '', true);
+        $this->lang->load(['welcome']);
+
+        $this->Session = $this->session->userdata('logged_in');
     }
 
+    /**
+     * Get the profile view for the logged in user.
+     *
+     * @return blade view.
+     */
     public function index()
     {
+        $userId = $this->Session['id'];
+        $data['user'] = Logins::find($userId);
 
+        $this->blade->render('auth/profile', $data);
     }
 
     public function updateSec()
