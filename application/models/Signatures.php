2b4f6537eb5e579961f0793286d2ced352804137
Tim Joosten <Tjoosten3@gmail.com>
2016-11-11 16:57:03 +0100

    Set Signatures Eloquent table

--- application/models/Signatures.php
+++ application/models/Signatures.php
@@ -1,1 +1,22 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Signatures extends Eloquent
+{
+    /**
+     * The database table.
+     *
+     * @var string
+     */
+    protected $table = 'Signatures';
+
+    /**
+     * Mass-assign fields
+     *
+     * @var array
+     */
+    protected $fillable = [''];
+
+
+}
