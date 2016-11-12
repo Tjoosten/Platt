d6575421bdbd102320eb8fcb252dea5188a62782
Tim Joosten <Tjoosten3@gmail.com>
2016-11-12 18:39:32 +0100

    Add functions

--- application/controllers/Signatures.php
+++ application/controllers/Signatures.php
@@ -1,6 +1,34 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Signatures extends MY_Controller
+/**
+ * Class Signature
+ */
+class Signature extends MY_Controller
 {
+    /**
+     * Signature constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(['url']);
+        $this->load->model('signatures', '', true);
+        $this->load->library(['session', 'form_validation']);
+    }
 
+    /**
+     * Insert a signature.
+     */
+    public function insert()
+    {
+
+    }
+
+    /**
+     * Destroy a invalid signature.
+     */
+    public function destroy()
+    {
+
+    }
 }
